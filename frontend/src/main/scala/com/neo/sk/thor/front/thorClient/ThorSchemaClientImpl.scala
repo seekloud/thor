d4184ccc7bb0b4d2147a6770831e389ae1146466
xusiran <xusiran@neotel.com.cn>
2018-11-15 18:31:14 +0900

    Client drawGame

--- frontend/src/main/scala/com/neo/sk/thor/front/thorClient/ThorSchemaClientImpl.scala
+++ frontend/src/main/scala/com/neo/sk/thor/front/thorClient/ThorSchemaClientImpl.scala
@@ -5,6 +5,8 @@ import com.neo.sk.thor.shared.ptcl.config.ThorGameConfig
 import com.neo.sk.thor.shared.ptcl.thor.ThorSchemaImpl
 import org.scalajs.dom
 
+import scala.collection.mutable
+
 /**
   * User: TangYaruo
   * Date: 2018/11/12
@@ -19,8 +21,17 @@ class ThorSchemaClientImpl (
 with AdventurerClient
 with FoodClient{
 
+//  protected val adventurerAttackAnimationMap: mutable.HashMap[Int, Int] = mutable.HashMap[Int, Int]() //可能存在的挥刀动画 id->动画id
+
   def drawGame(): Unit ={
-    drawAdventurer()
-    drawFood()
+    if(!waitSyncData){
+      adventurerMap.get(myId) match{
+        case Some(adventurer) =>
+          //TODO 各种环境绘画
+          drawAdventurer()
+          drawFood()
+        case None =>()
+      }
+    }
   }
 }
