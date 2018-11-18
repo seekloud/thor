ef7f3402de7f7e6d2ff650c04a1bb57b9b9abf45
fengjingyi <fengjingyi@neotel.com.cn>
2018-11-18 12:58:09 +0800

    debug

--- backend/src/main/scala/com/neo/sk/thor/core/thor/AdventurerServer.scala
+++ backend/src/main/scala/com/neo/sk/thor/core/thor/AdventurerServer.scala
@@ -3,9 +3,10 @@ package com.neo.sk.thor.core.thor
 import akka.actor.typed.ActorRef
 import akka.actor.typed.scaladsl.TimerScheduler
 import com.neo.sk.thor.core.RoomActor
-import com.neo.sk.thor.shared.ptcl.`object`.Adventurer
+import com.neo.sk.thor.shared.ptcl.`object`.{Adventurer, AdventurerImpl, AdventurerState}
 import com.neo.sk.thor.shared.ptcl.config.ThorGameConfig
 import com.neo.sk.thor.shared.ptcl.model.Point
+import com.neo.sk.thor.shared.ptcl.thor.ThorSchemaState
 
 /**
   * @author Jingyi
@@ -17,16 +18,23 @@ case class AdventurerServer(
                         config:ThorGameConfig,
                         playerId : String,
                         name : String,
-                        position: Point = Point(0, 0),
-                        level: Int = 1,
-                        energy: Int = 0,
-                        radius: Int = 1,
-                        direction: Float = 0,
-                        weaponLevel: Int = 1,
-                        weaponLength: Int = 1,
-                        speed: Float = 1,
-                        isSpeedUp: Boolean = false,
-                        killNum: Int = 0
+                        var position: Point = Point(0, 0),
+                        var level: Int = 1,
+                        var energy: Int = 0,
+                        var radiusLevel: Int = 1,
+                        var direction: Float = 0,
+                        var weaponLevel: Int = 1,
+                        var weaponLength: Float = 1,
+                        var speed: Float = 1,
+                        var isSpeedUp: Boolean = false,
+                        var killNum: Int = 0
                       ) extends Adventurer{
+
+  override var radius: Float = config.getAdventurerRadiusByLevel(radiusLevel)
+
+  def this(roomActor:ActorRef[RoomActor.Command], timer:TimerScheduler[RoomActor.Command],config: ThorGameConfig,adventurerState: AdventurerState){
+    this(roomActor, timer, config,adventurerState.playerId,adventurerState.name,adventurerState.position,adventurerState.level,adventurerState.energy,adventurerState.radiusLevel,adventurerState.direction,
+      adventurerState.weaponLevel,adventurerState.weaponLength, adventurerState.speed,adventurerState.isSpeedUp,adventurerState.killNum)
+  }
 
 }
