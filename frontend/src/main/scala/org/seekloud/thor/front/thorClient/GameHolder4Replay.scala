8a8e23c3933f23da7c5a25482d08e12b37a368db
TangYaruo <tangyaruo@neotel.com.cn>
2018-12-13 11:21:06 +0800

    debug

--- frontend/src/main/scala/org/seekloud/thor/front/thorClient/GameHolder4Replay.scala
+++ frontend/src/main/scala/org/seekloud/thor/front/thorClient/GameHolder4Replay.scala
@@ -19,6 +19,7 @@ class GameHolder4Replay(name: String, playerInfoOpt: Option[PlayerInfo] = None) 
   def startReplay(option: Option[ReplayInfo] = None) = {
     println(s"start replay...")
     if (firstCome) {
+      println(s"set loading---------1")
       gameState = GameState.replayLoading
       println(s"replay ws url: ${Routes.wsReplayGameUrl(option.get)}")
       websocketClient.setup(Routes.wsReplayGameUrl(option.get))
@@ -51,6 +52,7 @@ class GameHolder4Replay(name: String, playerInfoOpt: Option[PlayerInfo] = None) 
       case e: ThorGame.GridSyncState =>
         if (firstCome) {
           firstCome = false
+          println(s"set loading---------2")
           gameState = GameState.replayLoading
           thorSchemaOpt.foreach(_.receiveThorSchemaState(e.d))
         } else {
