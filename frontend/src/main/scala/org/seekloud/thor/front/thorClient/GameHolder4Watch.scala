734372bc0a32f6d14f0c0d44de54d2efeac7a69c
TangYaruo <tangyaruo@neotel.com.cn>
2018-12-17 17:17:56 +0800

    debug

--- frontend/src/main/scala/org/seekloud/thor/front/thorClient/GameHolder4Watch.scala
+++ frontend/src/main/scala/org/seekloud/thor/front/thorClient/GameHolder4Watch.scala
@@ -12,13 +12,13 @@ import org.seekloud.thor.shared.ptcl.thor.ThorSchemaClientImpl
   * @author Jingyi
   * @version 创建时间：2018/11/26
   */
-class GameHolder4Watch(name:String, roomId:Long, playerId: String, accessCode:String) extends GameHolder(name){
+class GameHolder4Watch(name: String, roomId: Long, playerId: String, accessCode: String) extends GameHolder(name) {
 
-//  override protected def gameLoop(): Unit = {
-//    handleResize
-//    thorSchemaOpt.foreach(_.update())
-//    logicFrameTime = System.currentTimeMillis()
-//  }
+  //  override protected def gameLoop(): Unit = {
+  //    handleResize
+  //    thorSchemaOpt.foreach(_.update())
+  //    logicFrameTime = System.currentTimeMillis()
+  //  }
 
   def watch() = {
     gameState = GameState.loadingPlay
@@ -26,36 +26,47 @@ class GameHolder4Watch(name:String, roomId:Long, playerId: String, accessCode:St
     websocketClient.setup(Routes.wsWatchGameUrl(roomId, playerId, accessCode))
   }
 
-  override protected def wsMessageHandler(data:WsMsgServer):Unit = {
+  override protected def wsMessageHandler(data: WsMsgServer): Unit = {
     //    println(data.getClass)
     data match {
-      case e:YourInfo =>
+      case e: YourInfo =>
         dom.console.log(s"$e")
+        startTime = System.currentTimeMillis()
         myId = e.id
         myName = e.name
         gameConfig = Some(e.config)
-        startTime = System.currentTimeMillis()
-        thorSchemaOpt = Some(ThorSchemaClientImpl(drawFrame, ctx, e.config, e.id, name,canvasBoundary,canvasUnit))
-        Shortcut.cancelSchedule(timer)
-        timer = Shortcut.schedule(gameLoop, e.config.frameDuration)
+        thorSchemaOpt = Some(ThorSchemaClientImpl(drawFrame, ctx, e.config, e.id, name, canvasBoundary, canvasUnit))
+        if (timer != 0) {
+          dom.window.clearInterval(timer)
+          thorSchemaOpt.foreach { grid => timer = Shortcut.schedule(gameLoop, grid.config.frameDuration) }
+        } else {
+          thorSchemaOpt.foreach { grid => timer = Shortcut.schedule(gameLoop, grid.config.frameDuration) }
+        }
         gameState = GameState.play
         nextFrame = dom.window.requestAnimationFrame(gameRender())
+        firstCome = false
 
 
-      case e: UserLeftRoom =>
-        if (e.playerId == myId) {
-          Shortcut.cancelSchedule(timer)
-          thorSchemaOpt.foreach(_.drawReplayMsg(s"玩家已经离开了房间，请重新选择观战对象"))
-        }
+
+      //      case e: UserLeftRoom =>
+      //        if (e.playerId == myId) {
+      //          Shortcut.cancelSchedule(timer)
+      //          thorSchemaOpt.foreach(_.drawReplayMsg(s"玩家已经离开了房间，请重新选择观战对象"))
+      //        }
 
       case e: GridSyncState =>
-//        println(s"still sync.but thorSchema is: $thorSchemaOpt")
+        //        println(s"still sync.but thorSchema is: $thorSchemaOpt")
         thorSchemaOpt.foreach(_.receiveThorSchemaState(e.d))
+        justSynced = true
 
-      case e:Ranks =>
+      case e: PingPackage =>
+        receivePingPackage(e)
+
+      case e: Ranks =>
+
         /**
           * 游戏排行榜
-          * */
+          **/
         currentRank = e.currentRank
         historyRank = e.historyRank
 
@@ -83,13 +94,21 @@ class GameHolder4Watch(name:String, roomId:Long, playerId: String, accessCode:St
         }
         thorSchemaOpt.foreach(_.receiveGameEvent(e))
 
-      case e:GameEvent =>
+      case e: GameEvent =>
+        e match {
+          case event: UserLeftRoom =>
+            if (event.playerId == myId) {
+              //              Shortcut.cancelSchedule(timer)
+              thorSchemaOpt.foreach(_.drawReplayMsg(s"玩家已经死亡，请重新选择观战对象"))
+            }
+          case _ =>
+        }
         thorSchemaOpt.foreach(_.receiveGameEvent(e))
 
-      case e:UserActionEvent =>
+      case e: UserActionEvent =>
         thorSchemaOpt.foreach(_.receiveUserEvent(e))
 
-      case RebuildWebSocket=>
+      case RebuildWebSocket =>
         thorSchemaOpt.foreach(_.drawReplayMsg("存在异地登录。。"))
         closeHolder
 
