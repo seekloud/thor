4cd89db1a001361c6d66098e4ce2932200eea6ab
liaosirui@neotel.com.cn <LSRPP667188521>
2018-12-19 18:11:19 +0800

    录像和音效加声音

--- frontend/src/main/scala/org/seekloud/thor/front/pages/WatchRender.scala
+++ frontend/src/main/scala/org/seekloud/thor/front/pages/WatchRender.scala
@@ -1,10 +1,13 @@
 package org.seekloud.thor.front.pages
 
 import mhtml.{Var, emptyHTML}
+
 import org.seekloud.thor.front.common.Page
 import org.seekloud.thor.front.thorClient.{GameHolder4Play, GameHolder4Watch}
 import org.seekloud.thor.front.utils.Shortcut
-import org.seekloud.thor.shared.ptcl.protocol.ThorGame.ThorGameInfo
+
+
+
 
 import scala.xml.Elem
 
@@ -16,7 +19,8 @@ class WatchRender(roomId: Long, playerId: String, accessCode: String)extends Pag
 
 
   private val canvas = <canvas id ="GameView" tabindex="1"></canvas>
-
+  private val audio_1 = <audio id="bgm-2" src="/thor/static/music/bgm-2.mp3" preload="auto"></audio>
+  private val audio_2 = <audio id="sound-4" src="/thor/static/music/sound-4.mp3" preload="auto"></audio>
   def init() = {
 
     val gameHolder = new GameHolder4Watch("GameView", roomId, playerId, accessCode)
@@ -30,6 +34,8 @@ class WatchRender(roomId: Long, playerId: String, accessCode: String)extends Pag
     Shortcut.scheduleOnce(() =>init(),0)
     <div>
       {canvas}
+      {audio_1}
+      {audio_2}
     </div>
   }
 
