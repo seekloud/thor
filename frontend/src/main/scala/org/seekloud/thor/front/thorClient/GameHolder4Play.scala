990da450bd7d22bac7f25b6b81424f8d82316f49
TangYaruo <tangyaruo@neotel.com.cn>
2018-12-28 10:55:03 +0800

    debug

--- frontend/src/main/scala/org/seekloud/thor/front/thorClient/GameHolder4Play.scala
+++ frontend/src/main/scala/org/seekloud/thor/front/thorClient/GameHolder4Play.scala
@@ -91,7 +91,7 @@ class GameHolder4Play(name: String, user: Option[UserInfo] = None) extends GameH
         barrage = s"${e.killerName}杀死了${e.name}"
         barrageTime = 300
         if (e.playerId == myId) {
-          println(s"隔500ms设置gameState为Stop!!!")
+//          println(s"隔500ms设置gameState为Stop!!!")
           dom.window.setTimeout(() => gameState = GameState.stop, 350)
           killer = e.killerName
           endTime = System.currentTimeMillis()
