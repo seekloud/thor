e14a0fd62c20e5bf8e6ae60f8d2046e862e9825d
fengjingyi <fengjingyi@neotel.com.cn>
2019-01-08 15:59:48 +0800

    restart debug

--- shared/src/main/scala/org/seekloud/thor/shared/ptcl/thor/ThorSchemaImpl.scala
+++ shared/src/main/scala/org/seekloud/thor/shared/ptcl/thor/ThorSchemaImpl.scala
@@ -113,7 +113,7 @@ class ThorSchemaImpl(
   protected def handleThorSchemaState(thorSchemaSate: ThorSchemaState) = {
     val curFrame = systemFrame
     val startTime = System.currentTimeMillis()
-    (curFrame until thorSchemaSate.f).foreach { _ =>
+    (math.max(curFrame, thorSchemaSate.f - 100) until thorSchemaSate.f).foreach { _ =>
       super.update()
       if (esRecoverSupport) addGameSnapshot(systemFrame, getThorSchemaState())
     }
