d7fac4708f40e3df9f9401f456076e19a05e4b71
TangYaruo <tangyaruo@neotel.com.cn>
2019-01-14 11:44:47 +0800

    rollback debug

--- shared/src/main/scala/org/seekloud/thor/shared/ptcl/thor/EsRecover.scala
+++ shared/src/main/scala/org/seekloud/thor/shared/ptcl/thor/EsRecover.scala
@@ -21,17 +21,17 @@ trait EsRecover {
     actionEventHistoryMap.put(frame, actionEvents)
   }
 
-  def addGameSnapshot(frame:Long,gameState:ThorSchemaState) = {
-    gameSnapshotMap.put(frame,gameState)
+  def addGameSnapshot(frame: Long, gameState: ThorSchemaState) = {
+    gameSnapshotMap.put(frame, gameState)
   }
 
-  def clearEsRecoverData():Unit = {
+  def clearEsRecoverData(): Unit = {
     gameEventHistoryMap.clear()
     actionEventHistoryMap.clear()
     gameSnapshotMap.clear()
   }
 
-  def rollback(frame:Int) = {
+  def rollback(frame: Int) = {
     require(frame < this.systemFrame)
 
     gameSnapshotMap.get(frame) match {
@@ -41,38 +41,38 @@ trait EsRecover {
         handleThorSchemaState(thorSchemaState)
         //同步所有数据
         removeKillInfoByRollback(frame)
-        (frame until curFrame).foreach{ f =>
-          this.addGameEvents(f,gameEventHistoryMap.getOrElse(f,Nil),actionEventHistoryMap.getOrElse(f,Nil))
+        (frame until curFrame).foreach { f =>
+          this.systemFrame = f
+          this.addGameEvents(f, gameEventHistoryMap.getOrElse(f, Nil), actionEventHistoryMap.getOrElse(f, Nil))
           this.rollbackUpdate()
         }
         val endTime = System.currentTimeMillis()
-        println(s"roll back to frame=${frame},nowFrame=${curFrame} use Time:${endTime - startTime}")
-      case None => println(s"there are not snapshot frame=${frame}")
+        println(s"roll back to frame=$frame, nowFrame=$curFrame use Time:${endTime - startTime}")
+      case None => println(s"there are not snapshot frame=$frame")
     }
   }
 
-  def rollback4GameEvent(e:GameEvent) = {
-//    println(s"roll back to frame=${e.frame},nowFrame=${systemFrame} because event:${e}")
+  def rollback4GameEvent(e: GameEvent) = {
+    //    println(s"roll back to frame=${e.frame},nowFrame=${systemFrame} because event:${e}")
     gameEventHistoryMap.put(e.frame, e :: gameEventHistoryMap.getOrElse(e.frame, Nil))
     rollback(e.frame)
   }
 
-  def rollback4UserActionEvent(e:UserActionEvent) = {
-//    println(s"roll back to frame=${e.frame},nowFrame=${systemFrame} because event:${e}")
+  def rollback4UserActionEvent(e: UserActionEvent) = {
+    //    println(s"roll back to frame=${e.frame},nowFrame=${systemFrame} because event:${e}")
     actionEventHistoryMap.put(e.frame, e :: actionEventHistoryMap.getOrElse(e.frame, Nil))
     rollback(e.frame)
   }
 
-  def removePreEventHistory(frame:Long, playerId:String, serialNum:Int):Unit = {
-    actionEventHistoryMap.get(frame).foreach{ actions =>
-      actionEventHistoryMap.put(frame,actions.filterNot(t => t.playerId == playerId && t.serialNum == serialNum))
+  def removePreEventHistory(frame: Long, playerId: String, serialNum: Int): Unit = {
+    actionEventHistoryMap.get(frame).foreach { actions =>
+      actionEventHistoryMap.put(frame, actions.filterNot(t => t.playerId == playerId && t.serialNum == serialNum))
     }
-}
-
-  def addUserActionHistory(e:UserActionEvent) = {
-    actionEventHistoryMap.put(e.frame, e :: actionEventHistoryMap.getOrElse(e.frame, Nil))
   }
 
+  def addUserActionHistory(e: UserActionEvent) = {
+    actionEventHistoryMap.put(e.frame, e :: actionEventHistoryMap.getOrElse(e.frame, Nil))
+  }
 
 
 }
