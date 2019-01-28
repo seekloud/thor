2513e75a3ca29dd30f64d4ed7c3ca3b643d53531
fengjingyi <fengjingyi@neotel.com.cn>
2019-01-28 21:36:40 +0800

    copyright

--- client/src/main/scala/org/seekloud/utils/CirceSupport.scala
+++ client/src/main/scala/org/seekloud/utils/CirceSupport.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Heiko Seeberger
+ * Copyright 2018 seekloud (https://github.com/seekloud)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
